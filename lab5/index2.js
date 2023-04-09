30a9a171553b9fb0ab8f96f90c912fbc430aac0c
danlemiu <duydanle@gmail.com>
2023-04-09 18:18:33 -0500

    commit lab4

--- lab5/index2.js
+++ lab5/index2.js
@@ -1,6 +1,7 @@
 "use strict";
 
-// 1
+// 1. Define a function max() that takes two numbers as arguments and returns the largest of them. 
+//Use the if-then- else construct available in Javascript.
 function max(num1, num2) {
     if(num1 > num2){
         return num1;
@@ -10,7 +11,7 @@ function max(num1, num2) {
 }
 console.log(max(10,3));
 
-//2
+//2. Define a function maxOfThree() that takes three numbers as arguments and returns the largest of them.
 function maxofThree(num1, num2, num3){
     // num1 = -Infinity;
     if(num1 >= num2 && num1 >= num3){
@@ -25,7 +26,7 @@ function maxofThree(num1, num2, num3){
 }
 console.log(maxofThree(1,3,2));
 
-//3
+//3. Write a function isVowel() that takes a character (i.e. a string of length 1) and returns true if it is a vowel, false otherwise.
 function isVowel(char){
     var vowels = ['a', 'e', 'i', 'o', 'u'];
     if(vowels.includes(char.toLowerCase())){
@@ -37,4 +38,23 @@ function isVowel(char){
 console.log(isVowel('b'));
 console.log(isVowel('u'));
 
-//4
+//4. Define a function sum() and a function multiply() that sums and multiplies (respectively) all the numbers in an input array of numbers. 
+//For example, sum([1,2,3,4]) should return 10, and multiply([1,2,3,4]) should return 24. Note/Hint: Do these using Imperative programming approach (i.e. for...loop or while...loop)
+function sum(numbers){
+    let sum = 0;
+    for(let i = 0; numbers.length; i++){
+        sum += numbers[i];
+    }
+    return sum;
+}
+
+function multiply(numbers){
+    let result = 1;
+    for (let i = 0; numbers.length; i++){
+        result *= numbers[i];
+    }
+    return result;
+}
+let myNumbers = [1,2,3,4];
+console.log(sum(myNumbers));
+console.log(multiply(myNumbers));
